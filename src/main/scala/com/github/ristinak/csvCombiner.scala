791598ab0b03fa6465eae55b1d7c9e40262e3c32
ristinak <ristinak@gmail.com>
2022-08-24 12:28:37 +0300

    Wrote csvCombiner to join two csv files downloaded from Yahoo finance.

---
+++ src/main/scala/com/github/ristinak/csvCombiner.scala
@@ -0,0 +1,43 @@
+package com.github.ristinak
+
+import com.github.ristinak.SparkUtil.{getSpark, readDataWithView}
+import org.apache.spark.sql.functions.{col, lit}
+
+object csvCombiner extends App {
+
+  val spark = getSpark("Sparky")
+
+  // getting the ticker from filepath
+
+  val filePath1 = "src/resources/NVDA.csv"
+  val ticker1 = filePath1.split("/").last.take(4)
+
+  val filePath2 = "src/resources/TWTR.csv"
+  val ticker2 = filePath2.split("/").last.take(4)
+
+  // saving dataframes
+
+  val dfOriginal1 = readDataWithView(spark, filePath1)
+  val dfOriginal2 = readDataWithView(spark, filePath2)
+
+  dfOriginal1.cache()
+  dfOriginal2.cache()
+
+  // dropping column 'Adj close' and adding column 'ticker'
+
+  val dfTicker1 = dfOriginal1.drop("Adj close").withColumn("ticker", lit(ticker1))
+  val dfTicker2 = dfOriginal2.drop("Adj close").withColumn("ticker", lit(ticker2))
+
+  // changing all column names to lowercase
+
+  val df1 = dfTicker1.select(dfTicker1.columns.map(x => col(x).as(x.toLowerCase)): _*)
+  val df2 = dfTicker2.select(dfTicker2.columns.map(x => col(x).as(x.toLowerCase)): _*)
+
+  // combining the two dataframes into one
+
+  val df = df1.union(df2).orderBy("date", "ticker")
+  df.sample(0.5).show(40)
+
+  df.write.mode("overwrite").option("header", true).csv("src/resources/twitter_nvidia.csv")
+
+}
