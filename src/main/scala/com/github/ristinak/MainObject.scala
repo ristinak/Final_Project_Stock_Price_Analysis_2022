e0a70836b88b27491094f7f5088ff2f33be5d530
ristinak <ristinak@gmail.com>
2022-08-29 11:28:02 +0300

    Added def main() in MainObject, redid scaladoc

--- src/main/scala/com/github/ristinak/MainObject.scala
+++ src/main/scala/com/github/ristinak/MainObject.scala
@@ -219,8 +219,9 @@ object MainObject {
     }
 
     /**
-     * @return prints dataframe with predicted close value of a stock
-     *         and saves best linear regression model to a directory
+     * @return builds linear regression model predicting next days close value and
+     * prints dataframe with predicted close value of a stock,
+     * saves best linear regression model to a directory
      * @param df         dataframe
      * @param printLines how many lines to print, default 20
      */
